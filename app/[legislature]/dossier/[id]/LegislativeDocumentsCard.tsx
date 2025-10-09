e355eff4765fef24275299d21a54d54f1653d4ea
alex <alex.fauquette@gmail.com>
2025-10-09 22:29:16 +0200

    affichar la chambre de provenance

--- app/[legislature]/dossier/[id]/LegislativeDocumentsCard.tsx
+++ app/[legislature]/dossier/[id]/LegislativeDocumentsCard.tsx
@@ -50,8 +50,11 @@ export const LegislativeDocumentsCard = async (
                   component={document.pdfUrl ? Link : "p"}
                   target="_blank"
                 >
-                  {document.titrePrincipalCourt}{" "}
-                  {document.pdfUrl && <LinkIcon sx={{ fontSize: "14px" }} />}
+                  {document.titrePrincipalCourt}
+                  {document.chambre ? ` (${document.chambre})` : "  "}
+                  {document.pdfUrl && (
+                    <LinkIcon sx={{ ml: 1, fontSize: "14px" }} />
+                  )}
                 </Typography>
               </Stack>
             );
