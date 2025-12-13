9f97f1a4386aa96eb3e9bb55a7372d91f161903e
alex <alex.fauquette@gmail.com>
2025-12-14 00:16:08 +0100

    Update header

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -20,16 +20,23 @@ export function NavBar({ navigation }: NavBarProps) {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
-            <div className="flex space-x-4 items-center justify-center">
+            <div className="flex justify-center">
+              <Link
+                href="/"
+                className={`flex items-center text-sm font-medium uppercase transition px-4 py-4 duration-300 ease-in-out text-gray-500 hover:bg-gray-200 hover:text-gray-700`}
+              >
+                <img
+                  src="/icon.png"
+                  alt="Nos Députés Logo"
+                  className="h-6 w-6 mr-2"
+                />
+                Nos Députés
+              </Link>
               {navigation.map((item) => (
                 <Link
                   key={item.name}
                   href={item.href}
-                  className={`flex items-center justify-center h-5 text-sm font-medium uppercase transition px-4 py-4 duration-300 ease-in-out transform rounded-2xl ${
-                    pathname === item.href
-                      ? "bg-black text-white"
-                      : "text-gray-500 hover:bg-gray-200 hover:text-gray-700"
-                  }`}
+                  className={`flex items-center text-sm font-medium uppercase transition px-4 py-4 duration-300 ease-in-out text-gray-500 hover:bg-gray-200 hover:text-gray-700`}
                 >
                   {item.name}
                 </Link>
