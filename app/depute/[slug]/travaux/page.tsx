9bb79661e752e5de6eae73c9dec56440045f5357
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2026-01-06 22:36:09 +0100

    clean: batch de petites amélioration du design (#61)

--- app/depute/[slug]/travaux/page.tsx
+++ app/depute/[slug]/travaux/page.tsx
@@ -71,13 +71,13 @@ export default function Travaux() {
 
   return (
     <Stack>
-      <Typography variant="h2">Travaux legislatifs</Typography>
-      <Typography variant="h3" sx={{ mb: 1, mt: 2 }}>
+      <Typography variant="subtitle1" component="h2" fontWeight={"bold"}>Travaux législatifs</Typography>
+      <Typography variant="h4" fontWeight="medium" sx={{ mb: 1, mt: 2 }}>
         Propositions de loi
       </Typography>
       {!propositionDeLoi || propositionDeLoi.length === 0 ? (
         <Typography variant="body2">
-          Aucune proposition de loi trouvée
+          Aucune proposition de loi trouvée.
         </Typography>
       ) : (
         <ul>
@@ -101,11 +101,11 @@ export default function Travaux() {
           ))}
         </ul>
       )}
-      <Typography variant="h3" sx={{ mb: 1, mt: 2 }}>
+      <Typography variant="h4" fontWeight="medium" sx={{ mb: 1, mt: 2 }}>
         Rapports
       </Typography>
       {!rapports || rapports.length === 0 ? (
-        <Typography variant="body2">Aucun rapport trouvé</Typography>
+        <Typography variant="body2">Aucun rapport trouvé.</Typography>
       ) : (
         <ul>
           {rapports.map((docs) => (
@@ -133,11 +133,11 @@ export default function Travaux() {
           ))}
         </ul>
       )}
-      <Typography variant="h3" sx={{ mb: 1, mt: 2 }}>
-        Resolutions
+      <Typography variant="h4" fontWeight="medium" sx={{ mb: 1, mt: 2 }}>
+        Résolutions
       </Typography>
       {!resolutions || resolutions.length === 0 ? (
-        <Typography variant="body2">Aucune resolution trouvée</Typography>
+        <Typography variant="body2">Aucune résolution trouvée.</Typography>
       ) : (
         <ul>
           {resolutions.map((docs) => (
@@ -166,11 +166,11 @@ export default function Travaux() {
         </ul>
       )}
 
-      <Typography variant="h3" sx={{ mb: 1, mt: 2 }}>
-        Dossier legislatifs initié
+      <Typography variant="h4" fontWeight="medium" sx={{ mb: 1, mt: 2 }}>
+        Dossier législatifs initié
       </Typography>
       {!dossiers || dossiers.length === 0 ? (
-        <Typography variant="body2">Aucune resolution trouvée</Typography>
+        <Typography variant="body2">Aucun dossier trouvé.</Typography>
       ) : (
         <ul>
           {dossiers.map((dossier) => (
