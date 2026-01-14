43118d0257d1654d5edfd83fc24ee132f274222d
FizBack <samhtsan@gmail.com>
2026-01-14 20:10:55 +0100

    Suppression du Somaire lorsque celui ci est vide (#68)
    Co-authored-by: Samuel Fillon <samuel@sommelierduparfum.com>

--- app/[legislature]/dossier/[id]/debat/[debatUid]/page.tsx
+++ app/[legislature]/dossier/[id]/debat/[debatUid]/page.tsx
@@ -46,25 +46,39 @@ export default async function Page({
     } as Record<string, number>
   );
 
-  return (
+  const sections = interventions.filter((p) =>
+    SUMMARY_CODES.has(p.codeGrammaire!)
+  );
+
+  const hasSummary = sections.length > 0;
+
+return (
     <>
+      { /* On n'affiche la colonne de gauche que s'il y a un sommaire associé */}
+      {hasSummary && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 24,
+            flex: 2,
+          }}
+        >
+          <DebateSummary sections={sections} />
+        </div>
+      )}
+
+      {/* On adapte le style de la colonne principale */}
       <div
         style={{
           display: "flex",
-          flexDirection: "row",
-          gap: 24,
-          flex: 2,
+          flexDirection: "column",
+          flex: 5,
+          margin: hasSummary ? "0" : "0 auto", 
+          maxWidth: hasSummary ? "none" : "750px",
+          width: "100%"
         }}
       >
-        <DebateSummary
-          // wordsCounts={wordsCounts}
-          sections={interventions.filter(
-            (p) =>
-              SUMMARY_CODES.has(p.codeGrammaire!) 
-          )}
-        />
-      </div>
-      <div style={{ display: "flex", flexDirection: "column", flex: 5 }}>
         <DebateTranscript
           title={debat?.dateSeanceJour ?? ""}
           paragraphes={interventions}
