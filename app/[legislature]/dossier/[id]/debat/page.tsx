884fb1fbc995573b94b35ef92e665e0104053988
Samuel Fillon <samuel@sommelierduparfum.com>
2026-01-24 18:36:16 +0100

    Fix problème de 404 si pas de redirection sur débats

--- app/[legislature]/dossier/[id]/debat/page.tsx
+++ app/[legislature]/dossier/[id]/debat/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <p>Ce dossier ne semble pas avoir fait l&apos;objet eu de débats</p>;
+// app/[legislature]/dossier/[id]/debat/page.tsx
+import { redirect } from "next/navigation";
+import { getDebats } from "@/data/getDebats";
+
+export default async function Page({ params }: { params: Promise<{ id: string, legislature: string }> }) {
+  const { id, legislature } = await params;
+  const debats = await getDebats(id);
+  
+  const debatsDisponibles = debats?.filter(d => d._count.paragraphes > 0);
+
+  if (debatsDisponibles && debatsDisponibles.length > 0) {
+    redirect(`/${legislature}/dossier/${id}/debat/${debatsDisponibles[0].uid}`);
+  }
+
+  return <p>Ce dossier ne semble pas avoir fait l'objet de débats.</p>;
 }
