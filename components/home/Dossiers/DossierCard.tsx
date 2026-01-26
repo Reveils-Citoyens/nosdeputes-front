92abe21c177ba0a3e09947eb8c19eb155de3b57c
alex <alex.fauquette@gmail.com>
2026-01-26 22:28:02 +0100

    bring back what i remove earlier (sorry)

--- components/home/Dossiers/DossierCard.tsx
+++ components/home/Dossiers/DossierCard.tsx
@@ -111,6 +111,9 @@ const DossierCard = (props: DossierCardProps) => {
             lineHeight: 1.4,
             overflow: "hidden",
             flexGrow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 3,
+            WebkitBoxOrient: "vertical",
           }}
         >
           {titre}
